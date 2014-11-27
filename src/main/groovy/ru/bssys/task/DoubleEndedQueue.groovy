859b2dfbd960b6cbbe8ba4d171c33edcd6c7cb4c
java5 <radik.zainullin@gmail.com>
2014-11-27 20:30:27 +0500

    03

---
+++ src/main/groovy/ru/bssys/task/DoubleEndedQueue.groovy
@@ -0,0 +1,14 @@
+package ru.bssys.task
+
+/**
+ * Created by radik on 27.11.2014.
+ * Двунаправленная очередь – структура данных, для которой доступны операции:
+    •	Добавить число в начало очереди; add first
+    •	Добавить число в конец очереди;  add last
+    •	Изъять число из начала очереди;  fetch first
+    •	Изъять число из конца очереди;   fetch last
+ */
+class DoubleEndedQueue {
+  private def stack = []
+
+}
---
+++ src/main/groovy/ru/bssys/task/strategy/addFirst.groovy
@@ -0,0 +1,8 @@
+package ru.bssys.task.strategy
+
+/**
+ * Created by radik on 27.11.2014.
+ */
+def addFirst = {
+  println 'add first'
+}
