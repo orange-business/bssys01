018d0555929e83f2162cb5e5f1efa648137556d0
java5 <radik.zainullin@gmail.com>
2014-11-28 02:06:02 +0500

    2014.11.28_01

--- src/main/groovy/ru/bssys/task/DoubleEndedQueue.groovy
+++ src/main/groovy/ru/bssys/task/DoubleEndedQueue.groovy
@@ -3,46 +3,45 @@ package ru.bssys.task
 /**
  * Created by radik on 27.11.2014.
  * Двунаправленная очередь – структура данных, для которой доступны операции:
-    •	Добавить число в начало очереди; add first
-    •	Добавить число в конец очереди;  add last
-    •	Изъять число из начала очереди;  fetch first
-    •	Изъять число из конца очереди;   fetch last
+ •	Добавить число в начало очереди; add first
+ •	Добавить число в конец очереди;  add last
+ •	Изъять число из начала очереди;  fetch first
+ •	Изъять число из конца очереди;   fetch last
  */
 class DoubleEndedQueue {
-  private def first, last, tmp, length = 0 as BigInteger
+  private def first, last, length = 0 as BigInteger
   def addFirst = { t ->
     if (!first) {
       first = last = t
+      first.metaClass.nxt = null
       length++
       return
     }
-    {->
-      tmp = first
-      first = t
-      first.metaClass.next = {-> tmp }
-      tmp.metaClass.previous = {-> first }
-      length++
-    }()
+    def second = first
+    first = t
+    first.metaClass.nxt = second
+    second.metaClass.prv = first
+    length++
     return
   }
   def fetchFirst = { ->
     if (!first) return
-    else {
-      {->
-        tmp = first
-        first = first.next()
-        length--
-      }()
-      return tmp
-    }
+    def deleted = first
+    first = first.nxt
+    if (first) first.prv = null
+    length--
+    return deleted
   }
+
   BigInteger getLength() { return length }
   String toString(){
-    String output = first + " " + first.next() + " " + (first.next()).next()
-//    while(first) {
-//      output += first?.next()
-//      first = first?.next()
-//    }
+    def curr = first
+    String output = ''
+    if (curr) output += curr.toString()
+    while(curr && curr.metaClass.hasProperty(curr, 'nxt') && curr.nxt != null) {
+      curr = curr.nxt
+      output += ' ' + curr.toString()
+    }
     return output
   }
 }
--- src/test/groovy/ru/bssys/task/DoubleEndedQueueTest.groovy
+++ src/test/groovy/ru/bssys/task/DoubleEndedQueueTest.groovy
@@ -6,21 +6,48 @@ import spock.lang.Specification
  * Created by radik on 27.11.2014.
  */
 class DoubleEndedQueueTest extends Specification {
-  def stack = new DoubleEndedQueue2()
-  void setup() {
+  def "test empty queue"() {
+    def stack = new DoubleEndedQueue()
+    expect: stack.length == 0 & stack.toString() == ''
+  }
+  def "test empty with fetchFirst method"() {
+    def stack = new DoubleEndedQueue()
+    stack.fetchFirst()
+    expect: stack.length == 0 & stack.toString() == ''
+  }
+
+  def "test fetchFirst once method"() {
+    def stack = new DoubleEndedQueue()
+    stack.addFirst(22)
+    stack.fetchFirst()
+    expect: stack.length == 0 & stack.toString() == ''
+  }
+
+  def "test addFirst once method Integer"() {
+    def stack = new DoubleEndedQueue()
+    stack.addFirst(-123456)
+    expect: stack.length == 1 & stack.toString() == '-123456'
+  }
+
+  def "test addFirst once method"() {
+    def stack = new DoubleEndedQueue()
+    stack.addFirst('HelloOne')
+    expect: stack.length == 1 & stack.toString() == 'HelloOne'
+  }
+  def "test addFirst method String"() {
+    def stack = new DoubleEndedQueue()
     stack.addFirst('HelloOne')
     stack.addFirst('HelloTwo')
     stack.addFirst('HelloThree')
     stack.addFirst('HelloFour')
+    expect: stack.length == 4 & stack.toString() == 'HelloFour HelloThree HelloTwo HelloOne'
   }
-  def "test addFirst method"() {
-    println stack
-    expect: stack.length == 4
+  def "test addFirst method Integer"() {
+    def stack = new DoubleEndedQueue()
+    stack.addFirst(15)
+    stack.addFirst(14)
+    stack.addFirst(2)
+    stack.addFirst(9)
+    expect: stack.length == 4 & stack.toString() == '9 2 14 15'
   }
-//  def "test fetchFirst method"() {
-//    def first = stack.fetchFirst()
-//    def second = stack.fetchFirst()
-//    def third = stack.fetchFirst()
-//    expect: first == 'Hello3' & second == 'Hello2' & third == 'Hello1'
-//  }
 }
